be39fff2bf5a90a2ce564f5e2f81847a70f0de14
Steve Graves <steve.graves86@gmail.com>
2017-01-23 15:16:19 -0800

    Implement board as object

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
+ var prompt = require('prompt');
+
 console.log('TIC TAC TOE');
 
-var board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+var board = {
+  1: ' ',
+  2: ' ',
+  3: ' ',
+  4: ' ',
+  5: ' ',
+  6: ' ',
+  7: ' ',
+  8: ' ',
+  9: ' '
+};
 
 var printBoard = function() {
-  console.log(board[0][0] + ' ' + board[0][1] + ' ' + board[0][2] + '\n' +
-              board[1][0] + ' ' + board[1][1] + ' ' + board[1][2] + '\n' +
-              board[2][0] + ' ' + board[2][1] + ' ' + board[2][2] + '\n');
+  console.log(board[1] + ' | ' + board[2] + ' | ' + board[3] + '\n' +
+              '--------- \n' +
+              board[4] + ' | ' + board[5] + ' | ' + board[6] + '\n' +
+              '--------- \n' +
+              board[7] + ' | ' + board[8] + ' | ' + board[9] + '\n');
 };
 
 printBoard();
+
+var getPlay = function(player) {
+
+  printBoard();
+
+  prompt.start();
+
+  prompt.get(['position'], function (err, result) {
+    console.log(result.position);
+  });
+  
+}
